808944f7521c8b0ee32fec924800deb89cd9053c
Carlos Roa <cignacioroa@gmail.com>
2025-04-07 17:38:51 -0400

    added ruth api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const isProduction = import.meta.env.MODE === 'production';
 
 const API = axios.create({
-  baseURL: '/api', 
+  baseURL: isProduction
+    ? 'https://flyme-5dxt.onrender.com/api'  
+    : 'http://localhost:5000/api',           
 });
 
-  
 // Enviar token automáticamente si existe
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -16,14 +18,8 @@ API.interceptors.request.use((config) => {
 });
 
 export const getFlights = () => API.get('/flights');
-export const bookFlight = (flightId, data) => {
-    return API.post('/bookings', {
-      flightId,
-      ...data, 
-    });
-  };
+export const bookFlight = (flightId, data) =>
+  API.post('/bookings', { flightId, ...data });
 export const getMyBookings = () => API.get('/bookings/me');
-export const cancelBooking = (bookingId) =>
-  API.delete(`/bookings/${bookingId}`);
+export const cancelBooking = (bookingId) => API.delete(`/bookings/${bookingId}`);
 export const getLocations = () => API.get("/locations");
-
