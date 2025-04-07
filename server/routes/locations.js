e75bfd318f5f35abf63709169f95db64fe3a5b93
Carlos Roa <cignacioroa@gmail.com>
2025-04-07 18:25:18 -0400

    added cors

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -5,14 +5,12 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const locations = await Flight.find().distinct("origin");
-    const destinations = await Flight.find().distinct("destination");
-    res.json({ locations, destinations });
+    const origins = await Flight.distinct("origin");
+    const destinations = await Flight.distinct("destination");
+    res.json({ origins, destinations });
   } catch (error) {
-    console.error("Error en /api/locations:", error.message);
     res.status(500).json({ message: "Error al obtener ubicaciones" });
   }
 });
-
 
 export default router;
