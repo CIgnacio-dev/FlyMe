5bc56ba1fe775c644006c309161e643b66feda4e
Carlos Roa <cignacioroa@gmail.com>
2025-04-07 16:40:11 -0400

    agregado vercel.json

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverApi: { version: '1', strict: true, deprecationErrors: true },
     });
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error de conexión: ${error.message}`);
